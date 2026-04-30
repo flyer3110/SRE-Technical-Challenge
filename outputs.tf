761fe3d59c27aa15204c631319bb1a234006c04e
Mike Greene <mikeg@Mikes-MacBook-Air.local>
2026-04-30 19:29:36 -0400

    outputs

--- outputs.tf
+++ outputs.tf
@@ -3,17 +3,17 @@ output "vpc_id" {
   value       = module.vpc.vpc_id
 }
 
-#output "alb_dns_name" {
-#  description = "DNS name of the Application Load Balancer"
-#  value       = aws_lb.app.dns_name
-#}
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.app.dns_name
+}
 
-#output "management_public_ip" {
-#  description = "Public IP of the management EC2 instance"
-#  value       = aws_instance.management.public_ip
-#}
+output "management_public_ip" {
+  description = "Public IP of the management EC2 instance"
+  value       = aws_instance.management.public_ip
+}
 
-#output "application_asg_name" {
-#  description = "Name of the application Auto Scaling Group"
-#  value       = aws_autoscaling_group.app.name
-#}
+output "application_asg_name" {
+  description = "Name of the application Auto Scaling Group"
+  value       = aws_autoscaling_group.app.name
+}
