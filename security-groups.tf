842b5b17fb466320e0fee080c59543fd5747d5ab
Mike Greene <mikeg@Mikes-MacBook-Air.local>
2026-04-30 12:18:39 -0400

    more files

---
+++ security-groups.tf
@@ -0,0 +1,73 @@
+resource "aws_security_group" "alb" {
+  name        = "poc-alb-sg"
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTP from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow outbound to application instances"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "management" {
+  name        = "poc-management-sg"
+  description = "Allow SSH from approved IP"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH from approved IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "app" {
+  name        = "poc-app-sg"
+  description = "Allow SSH from management and HTTP from ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description     = "SSH from management EC2"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.management.id]
+  }
+
+  ingress {
+    description     = "HTTP from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
